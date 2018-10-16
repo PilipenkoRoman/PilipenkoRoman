09d09a1e9a3e0d7100d5b05a93578ca2b954bec2
defabey <defabey@gmail.com>
2018-10-16 12:50:57 +0300

    add chromedriver

--- src/test/java/lesson1/SimpleTest.java
+++ src/test/java/lesson1/SimpleTest.java
@@ -1,10 +1,20 @@
 package lesson1;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class SimpleTest {
 
+    @Test
+    public void simpleTest(){
 
-    @Te
+    WebDriver driver=new ChromeDriver();
+  driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+
+}
+
 
 }
