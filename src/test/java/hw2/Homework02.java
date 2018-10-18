f94fcd56967acab3e404074e1118950bf8372e95
defabey <defabey@gmail.com>
2018-10-18 16:23:31 +0300

    test

--- src/test/java/hw2/Homework02.java
+++ src/test/java/hw2/Homework02.java
@@ -39,64 +39,6 @@ public class Homework02 {
     }
 
 
-    //
-//   @DataProvider(name = "test1")
-//   @DataProvider(name = "test2")
-//   @DataProvider(name = "test3")
-//   @DataProvider(name = "test4")
-    // @Test(dataProvider = "dp1" ,threadPoolSize=3,invocationCount=1)
-
-//    public static WebDriver driver = new ChromeDriver();
-
-//    @DataProvider(name="dp",parallel = true)
-//    public Object[][] dp() {
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-//        driver.navigate().to("https://epam.github.io/JDI/index.html");
-//        List<WebElement> textElements = driver.findElements(cssSelector(".benefit-txt"));
-////        return new Object[][]{
-////                new Object[]{textElements.get(0).getText(), "To include good practices\nand ideas from successful\nEPAM project"},
-////                new Object[]{textElements.get(1).getText(), "To be flexible and\ncustomizable"},
-////                new Object[]{textElements.get(2).getText(), "To be multiplatform"},
-////                new Object[]{textElements.get(3).getText(), "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…"}};
-//
-//  return new Object[][]{
-//          new Object[] {"To include good practices\nand ideas from successful\nEPAM project",},
-//          new Object[] {"To be flexible and\ncustomizable"},
-//          new Object[] {"To be multiplatform"},
-//          new Object[] {"Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…"}};}
-//
-//
-//  @Test(dataProvider = "dp")
-//    public void
-//
-//
-////    return Arrays.asList(
-////            "To include good practices\nand ideas from successful\nEPAM project",
-////            "To be flexible and\ncustomizable",
-////            "To be multiplatform",
-////            "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…");
-////
-////    //open BR
-////
-////}
-////
-////    //@DataProvider(parallel = true)
-////    public void indexPage4text() {
-////
-////        driver.manage().window().maximize();
-////        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-////        driver.navigate().to("https://epam.github.io/JDI/index.html");
-//////
-//////        assertEquals(driver.findElements(cssSelector(".benefit-txt")).stream()
-//////                        .map(WebElement::getText).collect(Collectors.toList()),
-//////                Arrays.asList(
-//////                        "To include good practices\nand ideas from successful\nEPAM project",
-//////                        "To be flexible and\ncustomizable",
-//////                        "To be multiplatform",
-//////                        "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…"));
-//////
-
 }
 
 
