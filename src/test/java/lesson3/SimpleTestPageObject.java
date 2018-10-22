8da3b12777a26b52d7219cea779e80198e6cb8f1
defabey <defabey@gmail.com>
2018-10-22 12:22:27 +0300

    test

--- src/test/java/lesson3/SimpleTestPageObject.java
+++ src/test/java/lesson3/SimpleTestPageObject.java
@@ -7,22 +7,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pageObjects.HomePage;
 
 import java.util.concurrent.TimeUnit;
 
+import static enums.Users.PITER_CHALOVSKII;
 import static org.testng.Assert.assertEquals;
 
 public class SimpleTestPageObject extends TestBase {
 
-    private WebDriver driver = new ChromeDriver();  ;
-    private HomePage  homePage= PageFactory.initElements(driver,HomePage.class);
+    private WebDriver driver;
+    private HomePage homePage;
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeClass
+    public void beforeClass() {
+        driver = new ChromeDriver();
+        homePage = PageFactory.initElements(driver, HomePage.class);
+    }
+
+    @BeforeMethod
     public void beforeMethod() {
-
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
     }
@@ -35,18 +42,15 @@ public class SimpleTestPageObject extends TestBase {
     @Test
     public void simpleTest() {
         //2 Navigate
-        driver.navigate().to("https://epam.github.io/JDI/index.html");
+        homePage.open(driver);
 
         //3 Assert Title
-        assertEquals(driver.getTitle(), "Home Page");
+        homePage.checkTitle(driver);
 
         //4 Login
-              homePage.login("epam","1234");
+        homePage.login(PITER_CHALOVSKII.login, PITER_CHALOVSKII.password);
 
-
-
-
-        WebElement mainTitle = driver.findElement(By.cssSelector("h3.main-title"));
-        assertEquals(mainTitle.getText(), "EPAM FRAMEWORK WISHES…");
+        //5 Check main title
+        homePage.checkMainText();
     }
 }
