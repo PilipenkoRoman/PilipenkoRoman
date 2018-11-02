bd3b7d919332bf61beb249e30946b1ea3e4b0b43
defabey <defabey@gmail.com>
2018-11-02 17:39:49 +0300

    test

--- src/main/java/hw4/DatesPage.java
+++ src/main/java/hw4/DatesPage.java
@@ -1,7 +1,6 @@
 package hw4;
 
 import com.codeborne.selenide.SelenideElement;
-import io.qameta.allure.Step;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
@@ -33,7 +32,6 @@ public class DatesPage {
     private SelenideElement profileName;
 
 
-
     @FindBy(css = "ul.uui-navigation.nav.navbar-nav.m-l8 > li.dropdown")
     private SelenideElement serviceMenu;
 
@@ -52,7 +50,7 @@ public class DatesPage {
 
     //================================methods===================================
 
-    @Step("Open JDI Test Application - Home Page")
+
     public void openPage() {
         open("https://epam.github.io/JDI");
     }
@@ -67,23 +65,7 @@ public class DatesPage {
     public void openDatesPage() {
         serviceMenu.click();
         datesPage.click();
-//        List<WebElement> listWeb = new ArrayList<>(getWebDriver().findElements(By.xpath("//*[@class='ui-slider-handle ui-state-default ui-corner-all']")));
-//        WebElement leftSlider = listWeb.get(0);
-//        WebElement rightSlider = listWeb.get(1);
-//        Actions builder = new Actions(getWebDriver());
-     }
-
-//    public void setRange(int left,int right){
-//        List<WebElement> webElementList = getWebDriver().findElements(By.xpath("//*[@class='ui-slider-handle ui-state-default ui-corner-all']"));
-//        WebElement rightSlider =webElementList.get(1);
-//        WebElement leftSlider = webElementList.get(0);
-//        left<leftSlider.get ? left=-left};
-//        Actions builder = new Actions(getWebDriver());
-//        builder.dragAndDropBy(left,74,0).perform();
-//        builder.dragAndDropBy(right,0,0).perform();
-//    }
-
-    //
+    }
 
     private double getStep() {
         return (double) slider.getSize().getWidth() / 100;
@@ -97,161 +79,14 @@ public class DatesPage {
         actions().dragAndDropBy(slider.toWebElement(), (int) shift, 0).build().perform();
     }
 
-    //
-////      if (leftSli.getLocation().getX() == rightSli.getLocation().getX() &&
-////            rightSli.getLocation().getX() == 1156) {
-//
+
     public void setRange(double leftSlider, double rightSlider) {
-//        SelenideElement leftSli = sliders.get(0);
-//        SelenideElement rightSli = sliders.get(1);
-        if (sliders.get(0).getLocation().getX() == sliders.get(1).getLocation().getX()){
+        if (sliders.get(0).getLocation().getX() == sliders.get(1).getLocation().getX()) {
             moveSlider(sliders.get(0), leftSlider);
             moveSlider(sliders.get(1), rightSlider);}
         moveSlider(sliders.get(1), rightSlider);
         moveSlider(sliders.get(0), leftSlider);
     }
-
-//    private double getStep() {
-//        return (double) slider.getSize().getWidth() / 100;
-//    }
-//
-//
-//    public void setRange(double from, double to) {
-//        // (leftPos<(leftSlider.getLocation().getX()/3.75))?leftPos=-leftPos:
-//        if (sliders.get(0).getLocation().getX() != sliders.get(1).getLocation().getX()) {
-//            moveSlider(sliders.get(0), from);
-//            moveSlider(sliders.get(1), to);
-//        }
-//        moveSlider(sliders.get(1), to);
-//        moveSlider(sliders.get(0), from);
-//   }
-//
-//    private void moveSlider(SelenideElement slider, double to) {
-//        double curPos = Double.parseDouble(slider.text());
-//        double shift = (to - curPos - 1) * getStep();
-//        actions().dragAndDropBy(slider.toWebElement(), (int) shift, 0).perform();
-//    }
-
-
-    public void setRange0to100() {
-//        List<WebElement> webElementList =getSliders();
-//        WebElement leftSlider = webElementList.get(0);
-//        WebElement rightSlider =webElementList.get(1);
-//        getSliders();
-////        Actions builder = new Actions(getWebDriver());
-//        System.out.println(rightSlider.getLocation().getX());
-//        System.out.println(leftSlider.getLocation().getX());
-//        builder.dragAndDropBy(leftSlider, -10000, 0).perform();
-//        System.out.println(leftSlider.getLocation().getX());
-//
-//        System.out.println(rightSlider.getLocation().getX());
-//        builder.dragAndDropBy(rightSlider, 10000, 0).perform();
-//        System.out.println(rightSlider.getLocation().getX());
-//        System.out.println("right");
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-////                builder.build();
-//    }
-//
-//    public void setRange0to0() {
-////        getSliders();
-//        Actions builder = new Actions(getWebDriver());
-//        System.out.println("asd");
-//        System.out.println(leftSlider.getLocation().getX());
-//        builder.dragAndDropBy(leftSlider, -1000, 0).perform();
-//        builder.dragAndDropBy(rightSlider, -2000, 0).perform();
-//        System.out.println(leftSlider.getLocation().getX());
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        builder.build();
-//        builder.dragAndDropBy(left,1000,0).perform();
-//        builder.dragAndDropBy(right,1000,0).perform();
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }}
-    }
-
-//    public void setRange100to100() {
-//        //    getSliders();
-////        Actions builder = new Actions(getWebDriver());
-//        builder.dragAndDropBy(rightSlider, 2000, 0).perform();
-//        builder.dragAndDropBy(leftSlider, 1000, 0).perform();
-//
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-////        builder.build();
-//        //        builder.build();
-//    }
-//
-//    public void setRange30to70() {
-////        List<WebElement> webElementList = getWebDriver().findElements(By.xpath("//*[@class='ui-slider-handle ui-state-default ui-corner-all']"));
-////        WebElement left = webElementList.get(0);
-////        WebElement right = webElementList.get(1);
-////        Actions builder = new Actions(getWebDriver());
-//        System.out.println(rightSlider.getLocation().getX());
-//        builder.dragAndDropBy(rightSlider, rightSlider.getLocation().getX() - 650, 0).perform();
-//        builder.dragAndDropBy(leftSlider, -111, 0).perform();
-////        builder.build();
-//        try {
-//            Thread.sleep(10000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-
-    //        List<WebElement> webElementList = driver.findElements(By.xpath("//*[@class='ui-slider-handle ui-state-default ui-corner-all']"));
-//        WebElement right =webElementList.get(1);
-//        WebElement left = webElementList.get(0);
-//       Actions builder = new Actions(getWebDriver());
-//        builder.dragAndDropBy(left,Integer.MIN_VALUE,0).perform();
-//        builder.dragAndDropBy(right,0,0).perform();
-
-//       builder.build();
-
-
-//    public ArrayList<SelenideElement> getSliders() {
-//        ElementsCollection selenideElements = $$(xpath("//*[@class='ui-slider-handle ui-state-default ui-corner-all']"));
-//        ArrayList<SelenideElement> selList = new ArrayList<SelenideElement>();
-//        selList.add(selenideElements.get(0));
-
-//        selList.add(selenideElements.get(1));
-
-//    return selList;
-
-//    }
-
-
-    //    @Step
-//    public void findDate() {
-//        serviceMenu.click();
-//        datesMenu.click();
-//    }
-
-//
-//    public  getSliders
-//    ElementsCollection selenideElements=$$(xpath("//*[@class='ui-slider-handle ui-state-default ui-corner-all']"));
-//    SelenideElement leftSlider=selenideElements.get(0);
-//    SelenideElement rightSlider=selenideElements.get(1);
-//
-//
-
-//            5
-//        driver.findElement(cssSelector("ul.uui-navigation.nav.navbar-nav.m-l8 > li.dropdown")).click();
-//                driver.findElement(cssSelector("li.dropdown.open > ul > li [href='dates.html']")).click();
-
 
     //================================checks===================================
 
@@ -259,46 +94,15 @@ public class DatesPage {
         assertEquals(getWebDriver().getTitle(), "Home Page");
     }
 
-    //
     public void checkRange(int from, int to) {
-
-        logsWindow.shouldHave(text(String.valueOf(to)));
         logsWindow.shouldHave(text(String.valueOf(from)));
+        logsWindow.shouldHave(text(String.valueOf(to)));
 
     }
-//
-//    public void check0to0() {
-//        $(logsWindow).shouldHave(text(TO_0.s));
-//        $(logsWindow).shouldHave(text(FROM_0.s));
-//    }
-//
-//    public void check100to100() {
-//        $(logsWindow).shouldHave(text(FROM_100.s));
-//        $(logsWindow).shouldHave(text(TO_100.s));
-//    }
-//
-//    public void check30to70() {
-//        $(logsWindow).shouldHave(text(TO_70.s));
-//        $(logsWindow).shouldHave(text(FROM_30.s));
-//    }
 
     public void checkProfileName() {
         profileName.shouldHave(text("Piter Chailovskii"));
     }
-
-
-//
-//        List<WebElement> webElementList = driver.findElements(By.xpath("//*[@class='ui-slider-handle ui-state-default ui-corner-all']"));
-//        WebElement left = webElementList.get(0);
-//        WebElement right = webElementList.get(1);
-//    }
-
-
-    //    @Step
-//    public void checkMainText() {
-//        //mainTitle.shouldBe(visible);
-//        //mainTitle.shouldHave(text("EPA FRAMEWORK WISHES..."));
-//    }
 
 
 }
--- src/test/java/hw4/DatesTestPageObject.java
+++ src/test/java/hw4/DatesTestPageObject.java
@@ -5,6 +5,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Configuration.startMaximized;
 import static com.codeborne.selenide.Selenide.page;
 import static enums.Users.PITER_CHALOVSKII;
 
@@ -16,13 +17,12 @@ public class DatesTestPageObject {
     public void beforeSuite() {
         Configuration.browser = "chrome";
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        startMaximized = true;
     }
 
     @BeforeClass
     public void beforeClass() {
         homePageSelenide = page(hw4.DatesPage.class);
-//        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-//        Configuration.browser = "CHROME";
     }
 
     @Test
@@ -37,80 +37,34 @@ public class DatesTestPageObject {
 //        3	Perform login
         homePageSelenide.login(PITER_CHALOVSKII.login, PITER_CHALOVSKII.password);
 
-//        4	Assert User name in the left-top side of screen that user is loggined
+//        4	Assert User name in the left-top side of screen that user is logged
         homePageSelenide.checkProfileName();
 
 //        5	Open through the header menu Service -> Dates Page
         homePageSelenide.openDatesPage();
 
-//        assertEquals(getWebDriver().findElements(cssSelector("header .nav > li")).stream()
-//                        .map(WebElement::getText).collect(Collectors.toList()),
-//                Arrays.asList("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS"));
-//        homePageSelenide.findDate();
+//        6	Using drag-and-drop set Range sliders. left sliders - the most left position, right slider - the most right position
+        homePageSelenide.setRange(0, 100);
 
-//        6	Using drag-and-drop set Range sliders. left sliders - the most left position, right slider - the most rigth position
-        homePageSelenide.setRange(0,100);
-//        homePageSelenide.setRange(0,80);
-//        homePageSelenide.setRange(30,100);
-//        homePageSelenide.setRange(15,60);
-//        homePageSelenide.setRange(25,75);
-
-      //  7	Assert that for "From" and "To" sliders there are logs rows with corresponding values
-      homePageSelenide.checkRange(0,100);
+        //  7	Assert that for "From" and "To" sliders there are logs rows with corresponding values
+        homePageSelenide.checkRange(0, 100);
 
 //        8	Using drag-and-drop set Range sliders. left sliders - the most left position, right slider - the most left position.
-        homePageSelenide.setRange(0,0);
+        homePageSelenide.setRange(0, 0);
 
 //        9	Assert that for "From" and "To" sliders there are logs rows with corresponding values
-     homePageSelenide.checkRange(0,0);
+        homePageSelenide.checkRange(0, 0);
 
-//        10	Using drag-and-drop set Range sliders. left sliders - the most rigth position, right slider - the most rigth position.
-        homePageSelenide.setRange(100,100);
+//        10	Using drag-and-drop set Range sliders. left sliders - the most rigth position, right slider - the most right position.
+        homePageSelenide.setRange(100, 100);
 
 //        11	Assert that for "From" and "To" sliders there are logs rows with corresponding values
-    homePageSelenide.checkRange(100,100);
+        homePageSelenide.checkRange(100, 100);
 
 //        12	Using drag-and-drop set Range sliders.
-        homePageSelenide.setRange(30,70);
+        homePageSelenide.setRange(30, 70);
 
 //        13	Assert that for "From" and "To" sliders there are logs rows with corresponding values
-     homePageSelenide.checkRange(30,70);
-//
-////        //   setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-////        //2 Navigate
-//        homePageSelenide.openPage();
-//
-//        //3 Assert Title
-//        homePageSelenide.checkTitle();
-//
-//        //4 Login
-//        homePageSelenide.login(PITER_CHALOVSKII.login, PITER_CHALOVSKII.password);
-//
-//        //5 Check main title
-//        homePageSelenide.checkMainText();
-//
-//
-//        //1
-//
-//        //2 Navigate
-//        open("https://epam.github.io/JDI/");
-//
-//        //3 Assert Title
-//        assertEquals(getWebDriver().getTitle(), "Home Page");
-//
-//        //4 Login
-//        $(".profile-photo").click();
-//        $("[id = 'Name']").sendKeys("epam");
-//        $("[id = 'Password']").sendKeys("1234");
-//        $("[type = 'submit']").click();
-//
-//
-//        SelenideElement mainTitle = $("h3.main-title");
-//
-//
-//        mainTitle.shouldBe(visible);
-//        mainTitle.shouldHave(text("EPAM FRAMEWORK WISHES…"));
-//
-
+        homePageSelenide.checkRange(30, 70);
     }
 }
