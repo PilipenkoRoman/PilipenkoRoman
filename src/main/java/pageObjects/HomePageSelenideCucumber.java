bbf034af1884ac5d8e0c765cf3b8f8a580558db9
defabey <defabey@gmail.com>
2018-11-09 09:42:40 +0300

    test

--- src/main/java/pageObjects/HomePageSelenideCucumber.java
+++ src/main/java/pageObjects/HomePageSelenideCucumber.java
@@ -2,16 +2,22 @@ package pageObjects;
 
 
 import com.codeborne.selenide.SelenideElement;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import io.qameta.allure.Step;
 import org.openqa.selenium.support.FindBy;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.page;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.testng.Assert.assertEquals;
 
-public class HomePageSelenide {
+public class HomePageSelenideCucumber {
+    public HomePageSelenideCucumber() {
+
+   page(this); }
 
     @FindBy(css = ".profile-photo")
     private SelenideElement profileButton;
@@ -31,12 +37,13 @@ public class HomePageSelenide {
     //================================methods===================================
 
     @Step("Open JDI Test Application - Home Page")
-
+    @When("I'm on the Home Page")
     public void openPage() {
         open("https://epam.github.io/JDI/index.html");
     }
 
     @Step
+    @When("I login as user (.+) with password (.+)")
     public void login(String name, String passwd) {
         profileButton.click();
         login.sendKeys(name);
@@ -46,9 +53,8 @@ public class HomePageSelenide {
 
     //================================checks===================================
 
-
-
     @Step
+    @Then("The browser title is Home Page")
     public void checkTitle() {
         assertEquals(getWebDriver().getTitle(), "Home Page");
     }
@@ -58,6 +64,14 @@ public class HomePageSelenide {
         mainTitle.shouldBe(visible);
         mainTitle.shouldHave(text("EPA FRAMEWORK WISHES�"));
     }
+
+    @Step
+    @Then("The user icon is displayed on the header")
+    public void checkUserIcon(){
+        profileButton.shouldBe(visible);
+    }
+
+
 
 
 }
